5386d8aed2283b5b6960c22fcda2d3cfbe591786
SeaseaSea3 <2211894501@qq.com>
2026-05-13 12:43:37 +0800

    二次修改

---
+++ BDZG/Assets/Code/Main/ScannerToolController.cs
@@ -0,0 +1,203 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScannerToolController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("病人区域")]
+    public RectTransform patientArea;
+
+    [Header("显示器屏幕")]
+    public GameObject scannerScreen;
+    public Image toolIcon;
+
+    [Header("7个道具")]
+    public ToolData[] tools;
+
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    private Vector2 startPosition;
+
+    private bool hasScanned = false;
+    private int currentToolIndex = 0;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponentInParent<Canvas>();
+        startPosition = rectTransform.anchoredPosition;
+
+        hasScanned = false;
+        currentToolIndex = 0;
+
+        if (scannerScreen != null)
+        {
+            scannerScreen.SetActive(false);
+        }
+
+        if (toolIcon != null)
+        {
+            toolIcon.gameObject.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (!hasScanned)
+        {
+            return;
+        }
+
+        SwitchToolByMouseWheel();
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ConfirmCurrentTool();
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (hasScanned)
+        {
+            return;
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (hasScanned)
+        {
+            return;
+        }
+
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        if (hasScanned)
+        {
+            return;
+        }
+
+        if (IsOverPatient())
+        {
+            ShowToolsOnScanner();
+        }
+        else
+        {
+            rectTransform.anchoredPosition = startPosition;
+        }
+    }
+
+    private bool IsOverPatient()
+    {
+        return RectTransformUtility.RectangleContainsScreenPoint(
+            patientArea,
+            Input.mousePosition,
+            canvas.worldCamera
+        );
+    }
+
+    private void ShowToolsOnScanner()
+    {
+        hasScanned = true;
+        currentToolIndex = 0;
+
+        if (scannerScreen != null)
+        {
+            scannerScreen.SetActive(true);
+        }
+
+        ShowCurrentTool();
+    }
+
+    private void SwitchToolByMouseWheel()
+    {
+        float wheel = Input.GetAxis("Mouse ScrollWheel");
+
+        if (wheel > 0f)
+        {
+            PreviousTool();
+        }
+        else if (wheel < 0f)
+        {
+            NextTool();
+        }
+    }
+
+    private void NextTool()
+    {
+        if (tools == null || tools.Length == 0)
+        {
+            Debug.LogError("Tools 道具列表为空！");
+            return;
+        }
+
+        currentToolIndex++;
+
+        if (currentToolIndex >= tools.Length)
+        {
+            currentToolIndex = 0;
+        }
+
+        ShowCurrentTool();
+    }
+
+    private void PreviousTool()
+    {
+        if (tools == null || tools.Length == 0)
+        {
+            Debug.LogError("Tools 道具列表为空！");
+            return;
+        }
+
+        currentToolIndex--;
+
+        if (currentToolIndex < 0)
+        {
+            currentToolIndex = tools.Length - 1;
+        }
+
+        ShowCurrentTool();
+    }
+
+    private void ShowCurrentTool()
+    {
+        if (tools == null || tools.Length == 0)
+        {
+            Debug.LogError("Tools 道具列表为空！");
+            return;
+        }
+
+        ToolData currentTool = tools[currentToolIndex];
+
+        if (toolIcon != null)
+        {
+            toolIcon.gameObject.SetActive(true);
+            toolIcon.sprite = currentTool.toolIcon;
+            toolIcon.preserveAspect = true;
+        }
+    }
+
+    private void ConfirmCurrentTool()
+    {
+        if (tools == null || tools.Length == 0)
+        {
+            Debug.LogError("Tools 道具列表为空！");
+            return;
+        }
+
+        ToolData currentTool = tools[currentToolIndex];
+
+        if (string.IsNullOrEmpty(currentTool.sceneName))
+        {
+            Debug.LogError("当前道具没有填写 Scene Name！");
+            return;
+        }
+
+        SceneManager.LoadScene(currentTool.sceneName);
+    }
+}
--- BDZG/Assets/Code/PJ/PuzzleManager.cs
+++ BDZG/Assets/Code/PJ/PuzzleManager.cs
@@ -2,86 +2,213 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    public static PuzzleManager Instance;
+    [Header("五个拼图块")]
+    public HexPuzzlePiece[] pieces;
 
-    [Header("拼图块")]
-    public Piece[] pieces;
+    [Header("开局是否随机旋转角度")]
+    public bool randomRotationOnStart = true;
 
-    [Header("目标位置")]
-    public Transform[] positions;
-
-    [Header("胜利UI（可选）")]
-    public GameObject winPanel;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
+    private int selectedIndex = 0;
+    private bool gameOver = false;
 
     void Start()
     {
-        Shuffle();
-    }
-    void OnEnable()
-    {
-        Shuffle();
-    }
-
-    // 打乱拼图
-    void Shuffle()
-    {
-        // 生成随机位置索引
-        int[] indexArr = new int[] { 0, 1, 2, 3 };
-
-        // Fisher-Yates 洗牌
-        for (int i = 0; i < indexArr.Length; i++)
+        if (randomRotationOnStart)
         {
-            int rand = Random.Range(i, indexArr.Length);
-            int temp = indexArr[i];
-            indexArr[i] = indexArr[rand];
-            indexArr[rand] = temp;
+            RandomRotateAllPieces();
         }
 
-        // 分配位置 + 随机旋转
+        SelectPiece(0);
+    }
+
+    void Update()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+
+        HandleSelectInput();
+        HandleRotateInput();
+    }
+
+    private void RandomRotateAllPieces()
+    {
+        foreach (HexPuzzlePiece piece in pieces)
+        {
+            piece.RandomRotation();
+        }
+    }
+
+    private void HandleSelectInput()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            MoveSelection(Vector2.up);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            MoveSelection(Vector2.down);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            MoveSelection(Vector2.left);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            MoveSelection(Vector2.right);
+        }
+    }
+
+    private void HandleRotateInput()
+    {
+        if (pieces == null || pieces.Length == 0)
+        {
+            return;
+        }
+
+        HexPuzzlePiece currentPiece = pieces[selectedIndex];
+
+        if (currentPiece.IsRotating())
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            currentPiece.RotateLeft(CheckFinish);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            currentPiece.RotateRight(CheckFinish);
+        }
+    }
+
+    private void MoveSelection(Vector2 direction)
+    {
+        int nextIndex = selectedIndex;
+
+        /*
+            Piece0        Piece3
+
+                   Piece2
+
+            Piece1        Piece4
+        */
+
+        if (selectedIndex == 0)
+        {
+            if (direction == Vector2.down)
+            {
+                nextIndex = 1;
+            }
+            else if (direction == Vector2.right)
+            {
+                nextIndex = 2;
+            }
+        }
+        else if (selectedIndex == 1)
+        {
+            if (direction == Vector2.up)
+            {
+                nextIndex = 0;
+            }
+            else if (direction == Vector2.right)
+            {
+                nextIndex = 2;
+            }
+        }
+        else if (selectedIndex == 2)
+        {
+            if (direction == Vector2.left)
+            {
+                nextIndex = 0;
+            }
+            else if (direction == Vector2.right)
+            {
+                nextIndex = 3;
+            }
+            else if (direction == Vector2.down)
+            {
+                nextIndex = 1;
+            }
+            else if (direction == Vector2.up)
+            {
+                nextIndex = 0;
+            }
+        }
+        else if (selectedIndex == 3)
+        {
+            if (direction == Vector2.down)
+            {
+                nextIndex = 4;
+            }
+            else if (direction == Vector2.left)
+            {
+                nextIndex = 2;
+            }
+        }
+        else if (selectedIndex == 4)
+        {
+            if (direction == Vector2.up)
+            {
+                nextIndex = 3;
+            }
+            else if (direction == Vector2.left)
+            {
+                nextIndex = 2;
+            }
+        }
+
+        SelectPiece(nextIndex);
+    }
+
+    private void SelectPiece(int index)
+    {
+        if (pieces == null || pieces.Length == 0)
+        {
+            return;
+        }
+
         for (int i = 0; i < pieces.Length; i++)
         {
-            int posIndex = indexArr[i];
-
-            pieces[i].SetPosition(posIndex, positions[posIndex].position);
-
-            int rot = Random.Range(0, 4) * 90;
-            pieces[i].SetRotation(rot);
+            pieces[i].SetSelected(false);
         }
 
-        if (winPanel != null)
-            winPanel.SetActive(false);
+        selectedIndex = Mathf.Clamp(index, 0, pieces.Length - 1);
+
+        pieces[selectedIndex].SetSelected(true);
     }
 
-    // 检查胜利
-    public void CheckWin()
+    private void CheckFinish()
     {
-        foreach (Piece p in pieces)
+        if (AllCorrect())
         {
-            if (!p.IsCorrect())
-                return;
+            gameOver = true;
+
+            Debug.Log("游戏结束：所有拼图都旋转回原来的角度！");
+
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                pieces[i].SetSelected(false);
+            }
+        }
+    }
+
+    private bool AllCorrect()
+    {
+        foreach (HexPuzzlePiece piece in pieces)
+        {
+            if (!piece.IsCorrect())
+            {
+                return false;
+            }
         }
 
-        Win();
-    }
-
-    void Win()
-    {
-        Debug.Log("拼图完成！");
-
-        if (winPanel != null)
-            winPanel.SetActive(true);
-
-        // 延迟退出（更自然）
-        Invoke("ExitGame", 1.5f);
-    }
-
-    void ExitGame()
-    {
-        UIManager.Instance.ExitPuzzle();
+        return true;
     }
 }
